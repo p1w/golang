00e26058c3d3423fc0cb1856cf024e238306e0fb
Wooldridge <hn241342@oxfordshire.gov.uk>
2018-11-07 08:48:22 +0000

    more web examples

--- src/webServer/2-server.go
+++ src/webServer/2-server.go
@@ -1,3 +1,5 @@
+//This example shows how to handle dynamic and static (css, js etc) requests
+
 package main
 
 import (
@@ -6,12 +8,17 @@ import (
 )
 
 func main() {
+	//handle dynamic requests
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
 	})
 
+	//static requests  e.g localhost/static/page.css (and page.css exists)
+	//fs file server location
 	fs := http.FileServer(http.Dir("static/"))
+	//return the request page from the fs
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
+	//http listener
 	http.ListenAndServe(":8080", nil)
 }
--- src/webServer/basicWebServer.go
+++ src/webServer/basicWebServer.go
@@ -6,9 +6,14 @@ import (
 )
 
 func main() {
+
+	//the webserver in oneline. "/"  is the url to listen for. the rest is always the same
+	
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//the response from the server. 
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
+	//listen for traffic on port 8080.  nil is the default router
 	http.ListenAndServe(":8080", nil)
 }
